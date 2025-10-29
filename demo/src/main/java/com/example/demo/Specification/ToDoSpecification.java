ede59b9da04105961917571b823a03bb99fa08bd
bpraneeth15 <99836323+bpraneeth15@users.noreply.github.com>
2025-10-29 01:41:47 -0500

    Implemented the Dynamic criteria Filtering in the ToDoSpecification class

--- demo/src/main/java/com/example/demo/Specification/ToDoSpecification.java
+++ demo/src/main/java/com/example/demo/Specification/ToDoSpecification.java
@@ -4,6 +4,10 @@ import com.example.demo.model.ToDo;
 import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class ToDoSpecification {
 
     public static Specification<ToDo> findByCriteria(Boolean status, String dueDate, String taskDesc){
@@ -12,21 +16,26 @@ public class ToDoSpecification {
             //Implements the toPredicate method (the logic for how to build the actual database condition)
             @Override
             public Predicate toPredicate(Root<ToDo> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-                //some logic to build a predicate
-                //we build this using 3 objects, root, criteriaQuery and criteriaBuilder
-                //root-> table/entity that we are querying and its columns , example root.get("status"), root.get("taskDesc")
-                //get() method -> <Y> Path<Y> get(String attributeName); this returns a path object that points to the field(mentioned as the attributeName) in the database that is
-                //It’s generic: you decide what Y is, based on the field’s type.
-                // Path<Y> object that represents that field (or “path”) in the query.
-                //root.get("status") → returns a Path<Boolean>, root.get("taskDesc") → returns a Path<String>
-                //
-                Predicate predicateStatus = criteriaBuilder.equal(root.get("status"), status); //the equal method returns a predicate
-                Predicate predicateTaskDesc = criteriaBuilder.like(root.get("taskDesc"), taskDesc);//even if we use like, this predicate behaves exactly as equal because there are no wildcards being used
-               //lower method signature:- Expression<String> lower(Expression<String> var1);
-                //“Take the column represented by this Path object, and wrap it in the SQL function LOWER(...).”
-                //result is a new Expression object representing LOWER(task_desc).
-                //Predicate predicateDueDate = criteriaBuilder.like(root.get("taskDesc"), taskDesc);
-                return criteriaBuilder.or(predicateStatus, predicateTaskDesc);}
+
+                //create a list for storing each predicate that we want to use given the field names in the query
+                List<Predicate> predicateList = new ArrayList<>();
+
+                //step 2 : add each prediccacte to the list based on the giveen field names in the query
+                if(status != null){
+                    //if the status is not null then add the predicate to the list
+                    predicateList.add(criteriaBuilder.equal(root.get("status"), status));
+                }
+
+                if(taskDesc != null && !taskDesc.trim().isEmpty() ){
+                    //“Add a new condition that checks whether the lowercase version of taskDesc in the database contains the lowercase text given by the user, anywhere inside it.”
+                    predicateList.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("taskDesc")),"%"+taskDesc.toLowerCase(Locale.ROOT)+"%"));
+                }
+
+                if(dueDate != null && !dueDate.trim().isEmpty()){
+                    predicateList.add(criteriaBuilder.equal(root.get("dueDate"), dueDate));
+                }
+
+                return criteriaBuilder.and(predicateList.toArray(new Predicate[0]));}
         };
 
     }
